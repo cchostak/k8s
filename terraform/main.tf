568bca544f468c42ff54ce87d440c256920268c2
Christian Chostak <christian.chostak@devoteam.com>
2020-04-29 22:10:12 +0100

    dead end

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,19 @@ module "my-cluster" {
     }
   ]
 }
+
+module "vpc" {
+  source = "nekochans/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  nat_instance_ami         = "ami-0af1df87db7b650f4"
+  nat_instance_type        = "t2.micro"
+  nat_instance_volume_type = "gp2"
+  nat_instance_volume_size = "30"
+
+  azs             = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  private_subnets = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24"]
+  public_subnets  = ["10.10.10.0/24", "10.10.11.0/24", "10.10.12.0/24"]
+}
